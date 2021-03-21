b63ac00a81094db103969037a96436c4481f03d9
Param Thakkar <pht0104@gmail.com>
2021-03-21 14:21:10 -0400

    Display clicks + creation date

--- info.js
+++ info.js
@@ -13,10 +13,7 @@ window.onload = function () {
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 301) {
       data = JSON.parse(this.responseText);
-      document.querySelector("#long").innerHTML = data.long;
-      document.querySelector("#long").href = data.long;
-      document.querySelector("#long").style.textShadow = "none";
-      document.querySelector("#long").style.color = "white";
+      displayInfo(data);
     } else if (this.readyState == 4 && this.status == 404) {
       document.querySelector("#long").style.display = "none";
       document.querySelector("#description").innerHTML =
@@ -27,3 +24,18 @@ window.onload = function () {
   xhttp.open("GET", "https://api.1pt.co/getInfo?url=" + url, true);
   xhttp.send();
 };
+
+function displayInfo(info) {
+  const date = Date.parse(info.date);
+  const now = Date.now();
+
+  document.querySelector("#long").innerHTML = data.long;
+  document.querySelector("#long").href = data.long;
+  document.querySelector("#long").style.textShadow = "none";
+  document.querySelector("#long").style.color = "white";
+
+  document.querySelector("#clicks").innerHTML = data.hits;
+  document.querySelector("#date").innerHTML = Math.round(
+    (now - date) / (1000 * 60 * 60 * 24)
+  );
+}
