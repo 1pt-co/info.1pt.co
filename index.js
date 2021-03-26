5c95f49cade245256eea17e3de3bf2da99a6c631
Param Thakkar <pht0104@gmail.com>
2021-03-26 17:10:44 -0400

    Create homepage

---
+++ index.js
@@ -0,0 +1,37 @@
+const input = document.querySelector("#input");
+
+// Click 'GO' button if the ENTER key is pressed
+input.addEventListener("keyup", function (event) {
+  event.preventDefault();
+  if (event.keyCode == 13) {
+    go();
+  }
+});
+
+function go() {
+  let short = "";
+
+  if (input.value.substr(0, 7) == "1pt.co/") short = input.value.substr(7);
+  else if (input.value.substr(0, 11) == "www.1pt.co/")
+    short = input.value.substr(11);
+  else if (input.value.substr(0, 15) == "https://1pt.co/")
+    short = input.value.substr(15);
+  else if (input.value.substr(0, 14) == "http://1pt.co/")
+    short = input.value.substr(14);
+  else if (input.value.substr(0, 19) == "https://www.1pt.co/")
+    short = input.value.substr(19);
+  else if (input.value.substr(0, 18) == "http://www.1pt.co/")
+    short = input.value.substr(18);
+
+  if (short !== "") window.location.href = "https://info.1pt.co/" + short;
+  else showError("Invalid URL!");
+}
+
+const showError = (title, description) => {
+  Swal.fire({
+    title: title,
+    text: description,
+    icon: "error",
+    confirmButtonText: "OK",
+  });
+};
