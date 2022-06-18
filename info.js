638762438c2f082580e81db8daf3a3bf0837ee48
Param Thakkar <pht0104@gmail.com>
2022-06-18 19:30:48 -0400

    Fix status code check

--- info.js
+++ info.js
@@ -11,7 +11,7 @@ window.onload = function () {
   var xhttp = new XMLHttpRequest();
 
   xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 301) {
+    if (this.readyState == 4 && this.status == 200) {
       data = JSON.parse(this.responseText);
       displayInfo(data);
       finishLoading();
